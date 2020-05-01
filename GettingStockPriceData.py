0b861e98830a3f5d84b5628e3165d29979fc23c0
Aiyappa <aiyappa02@gmail.com>
2020-05-01 16:08:56 +0530

    2

--- GettingStockPriceData.py
+++ GettingStockPriceData.py
@@ -4,11 +4,17 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
-style.use('ggplot')
+# style.use('ggplot')
+#
+# start=dt.datetime(2000,1,1)
+# end=dt.datetime(2016,12,31)
+#
+# df= web.DataReader('TSLA','yahoo',start,end)
+#
+# df.to_csv('tsla.csv')
+df = pd.read_csv("tsla.csv")
 
-start=dt.datetime(2000,1,1)
-end=dt.datetime(2016,12,31)
-
-df= web.DataReader('TSLA','yahoo',start,end)
-print(df.head())
-print(df.tail())
+df=df.set_index("Date")
+df['High'].plot()
+plt.show()
+#print(df.head(5))
